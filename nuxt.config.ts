beb7fda78a6ed7fe0d6aea37df1af3f8a4a2e808
khanhthanhdev <thanhkt27507@gmail.com>
2026-03-14 08:29:51 +0700

    exclude _studio route

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -91,6 +91,10 @@ export default defineNuxtConfig({
       code: 'vi',
       name: 'Tiếng Việt',
     }],
+    skipSettingLocaleOnNavigate: true,
+  },
+  routeRules: {
+    '/_studio/**': { isr: false },
   },
   studio: {
     repository: {
